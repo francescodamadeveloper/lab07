d8309e641903808bba1009aa76428a665455cddb
francesco dama <francesco.dama@studio.unibo.it>
2025-11-04 12:27:54 +0100

    added modified files

--- 72-anonymous-nested-enum/src/main/java/it/unibo/functional/Transformers.java
+++ 72-anonymous-nested-enum/src/main/java/it/unibo/functional/Transformers.java
@@ -54,7 +54,12 @@ public final class Transformers {
      * @return A transformed list where each input element is replaced with the produced elements
      */
     public static <I, O> List<O> transform(final Iterable<I> base, final Function<I, O> transformer) {
-        return null;
+        return flattenTransform(base, new Function<I,Collection<? extends O>>() {
+            public Collection<? extends O> call(I input) {
+                return List.of(transformer.call(input));
+            }
+            
+        });
     }
 
     /**
@@ -70,9 +75,16 @@ public final class Transformers {
      * @return A flattened list with the elements of each collection in the input
      */
     public static <I> List<? extends I> flatten(final Iterable<? extends Collection<? extends I>> base) {
-        return null;
-    }
+        
+        return flattenTransform(base, new Function<Collection<? extends I> , Collection<? extends I>>() {
 
+            @Override
+            public Collection<? extends I> call(Collection<? extends I> input) {
+                return input;
+            }
+            
+        });
+    }
     /**
      * A function that applies a test to each element of an {@link Iterable}, returning a list containing only the
      * elements that pass the test.
@@ -87,7 +99,17 @@ public final class Transformers {
      * @return A list containing only the elements that passed the test
      */
     public static <I> List<I> select(final Iterable<I> base, final Function<I, Boolean> test) {
-        return null;
+        return flattenTransform(base, new Function <I,Collection<? extends I>>() {
+
+            @Override
+            public Collection<? extends I> call(I input) {
+                if(test.call(input)){
+                    return List.of(input);
+                } else {
+                    return List.of();
+                }
+            }
+        });
     }
 
     /**
@@ -103,6 +125,12 @@ public final class Transformers {
      * @return A list containing only the elements that passed the test
      */
     public static <I> List<I> reject(final Iterable<I> base, final Function<I, Boolean> test) {
-        return null;
+        return select(base, new Function<I,Boolean>() {
+            @Override
+            public Boolean call(I input) {
+                return !test.call(input);
+            }
+            
+        });
     }
 }
--- 72-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
+++ 72-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
@@ -1,5 +1,4 @@
 package it.unibo.nestedenum;
-
 import java.util.Comparator;
 import java.util.Locale;
 import java.util.Objects;
@@ -8,6 +7,39 @@ import java.util.Objects;
  * Implementation of {@link MonthSorter}.
  */
 public final class MonthSorterNested implements MonthSorter {
+        private static final int SHORT_MONTH=28;
+        private static final int USUAL_MONTH=30;
+        private static final int LONG_MONTH=31;
+    public static enum Month{
+        JANUARY("January",LONG_MONTH),
+        FEBRUARY("February",SHORT_MONTH),
+        MARCH("March",LONG_MONTH),
+        APRIL("April",USUAL_MONTH),
+        MAY("May",LONG_MONTH),
+        JUNE("June",USUAL_MONTH),
+        JULY("July",LONG_MONTH),
+        AUGUST("August",LONG_MONTH),
+        SEPTEMBER("September",USUAL_MONTH),
+        OCTOBER("October",LONG_MONTH),
+        NOVEMBER("November",USUAL_MONTH),
+        DECEMBER("December",LONG_MONTH);
+        private final String ActualName;
+        private final int Days;
+        private Month(final String name, final int days){
+            this.ActualName=name;
+            this.Days=days;
+        }
+        public String getActualName(final Month month){
+            return month.ActualName;
+        }
+        public Month fromString(final String month){
+            for(final Month name : Month.values()){
+                if(name.getActualName(name).compareToIgnoreCase(month) == 0){
+                    return name;
+                } else if()
+            }
+        }
+    }
 
     @Override
     public Comparator<String> sortByDays() {
